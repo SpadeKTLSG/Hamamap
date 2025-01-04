42d784f87e085771a0ef629b83316975fbf05885
caowenxing <caowenxing@newland.com.cn>
2025-01-04 15:52:33 +0800

    find golds

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# 机密
+# Hamamap
 
-手撕等
+蛤蟆Map
+
+运行:
+
+```shell
+java -jar target/Hamamap-1.0-SNAPSHOT.jar
+```
--- src/main/java/org/spc/HashMapExample.java
+++ src/main/java/org/spc/HashMapExample.java
@@ -1,2 +1,15 @@
-package org.spc;public class HashMapExample {
+package org.spc;
+
+import java.util.HashMap;
+
+public class HashMapExample {
+
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+        map.put("key3", "value3");
+
+        map.forEach((key, value) -> System.out.println(key + ": " + value));
+    }
 }
