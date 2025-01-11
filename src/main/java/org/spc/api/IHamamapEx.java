df440d71f8337b35325377be3bf64955066eaeaf
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2025-01-12 00:40:53 +0800

    IHamamapEx.java OK
    Signed-off-by: caowenxing <caowenxing@newland.com.cn>

--- src/main/java/org/spc/api/IHamamapEx.java
+++ src/main/java/org/spc/api/IHamamapEx.java
@@ -4,7 +4,6 @@ package org.spc.api;
 import org.spc.impl.KVHolder;
 import org.spc.tool.collect;
 
-import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -14,9 +13,9 @@ public interface IHamamapEx<K, V> extends IHamamap<K, V> {
 
 
     /**
-     * Returns an unmodifiable {@link Map.Entry} containing the given key and value.
-     * These entries are suitable for populating {@code Map} instances using the
-     * {@link Map#ofEntries Map.ofEntries()} method.
+     * Returns an unmodifiable entry containing the KV
+     * <p>
+     * 返回一个不可修改的包含KV的entry (Actually not)
      */
     static <K, V> IHamaEntryEx<K, V> entry(K k, V v) {
         return new KVHolder<>(k, v);
@@ -25,7 +24,7 @@ public interface IHamamapEx<K, V> extends IHamamap<K, V> {
     /**
      * Returns an unmodifiable map containing no KV
      * <p>
-     * 返回一个不可修改的没有KV的map
+     * 返回一个不可修改的没有KV的map (Actually not)
      */
     @SuppressWarnings("unchecked")
     static <K, V> IHamamapEx<K, V> of() {
@@ -45,184 +44,31 @@ public interface IHamamapEx<K, V> extends IHamamap<K, V> {
 
     /**
      * If the K not linked to V, link it with V and return null, else returns the current V
-     *
-     * @return the previous V linked to K, or
-     * {@code null} if there was no mapping for the key.
-     * (A {@code null} return can also indicate that the map
-     * previously associated {@code null} with the key,
-     * if the implementation supports null values.)
-     * @throws UnsupportedOperationException if the {@code put} operation
-     *                                       is not supported by this map
-     *                                       (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @throws ClassCastException            if the key or value is of an inappropriate
-     *                                       type for this map
-     *                                       (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @throws NullPointerException          if the specified key or value is null,
-     *                                       and this map does not permit null keys or values
-     *                                       (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @throws IllegalArgumentException      if some property of the specified key
-     *                                       or value prevents it from being stored in this map
-     *                                       (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @implSpec The default implementation is equivalent to, for this {@code map}:
-     *
-     * <pre> {@code
-     * V v = map.get(key);
-     * if (v == null)
-     *     v = map.put(key, value);
-     *
-     * return v;
-     * }</pre>
-     *
-     * <p>The default implementation makes no guarantees about synchronization
-     * or atomicity properties of this method. Any implementation providing
-     * atomicity guarantees must override this method and document its
-     * concurrency properties.
-     * @since 1.8
+     * <p>
+     * 若K没有关联V, 则关联他们并返回null, 否则返回当前V
      */
     default V putIfAbsent(K key, V value) {
-        V v = get(key);
-        if (v == null) {
-            v = put(key, value);
+        V v;
+        if ((v = get(key)) == null) {
+            put(key, value);
+            return null;
         }
-
         return v;
     }
 
 
     /**
-     * Removes the entry for the specified key only if it is currently
-     * mapped to the specified value.
-     *
-     * @param key   key with which the specified value is associated
-     * @param value value expected to be associated with the specified key
-     * @return {@code true} if the value was removed
-     * @throws UnsupportedOperationException if the {@code remove} operation
-     *                                       is not supported by this map
-     *                                       (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @throws ClassCastException            if the key or value is of an inappropriate
-     *                                       type for this map
-     *                                       (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @throws NullPointerException          if the specified key or value is null,
-     *                                       and this map does not permit null keys or values
-     *                                       (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @implSpec The default implementation is equivalent to, for this {@code map}:
-     *
-     * <pre> {@code
-     * if (map.containsKey(key) && Objects.equals(map.get(key), value)) {
-     *     map.remove(key);
-     *     return true;
-     * } else
-     *     return false;
-     * }</pre>
-     *
-     * <p>The default implementation makes no guarantees about synchronization
-     * or atomicity properties of this method. Any implementation providing
-     * atomicity guarantees must override this method and document its
-     * concurrency properties.
-     * @since 1.8
+     * Removes the entry for K only if it's mapped to the V, returns T if removed but F if not
+     * <p>
+     * 只有K映射到V时才移除K, 返回T移除, F未移除
      */
-    default boolean remove(Object key, Object value) {
-        Object curValue = get(key);
+    default boolean removeIfAbsent(K key, V value) {
+        V curValue = get(key);
         if (!Objects.equals(curValue, value) || (curValue == null && !containsKey(key))) {
             return false;
         }
         remove(key);
         return true;
-    }
-
-
-    /**
-     * Replaces the entry for the specified key only if currently
-     * mapped to the specified value.
-     *
-     * @param key      key with which the specified value is associated
-     * @param oldValue value expected to be associated with the specified key
-     * @param newValue value to be associated with the specified key
-     * @return {@code true} if the value was replaced
-     * @throws UnsupportedOperationException if the {@code put} operation
-     *                                       is not supported by this map
-     *                                       (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @throws ClassCastException            if the class of a specified key or value
-     *                                       prevents it from being stored in this map
-     * @throws NullPointerException          if a specified key or newValue is null,
-     *                                       and this map does not permit null keys or values
-     * @throws NullPointerException          if oldValue is null and this map does not
-     *                                       permit null values
-     *                                       (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @throws IllegalArgumentException      if some property of a specified key
-     *                                       or value prevents it from being stored in this map
-     * @implSpec The default implementation is equivalent to, for this {@code map}:
-     *
-     * <pre> {@code
-     * if (map.containsKey(key) && Objects.equals(map.get(key), oldValue)) {
-     *     map.put(key, newValue);
-     *     return true;
-     * } else
-     *     return false;
-     * }</pre>
-     * <p>
-     * The default implementation does not throw NullPointerException
-     * for maps that do not support null values if oldValue is null unless
-     * newValue is also null.
-     *
-     * <p>The default implementation makes no guarantees about synchronization
-     * or atomicity properties of this method. Any implementation providing
-     * atomicity guarantees must override this method and document its
-     * concurrency properties.
-     * @since 1.8
-     */
-    default boolean replace(K key, V oldValue, V newValue) {
-        Object curValue = get(key);
-        if (!Objects.equals(curValue, oldValue) || (curValue == null && !containsKey(key))) {
-            return false;
-        }
-        put(key, newValue);
-        return true;
-    }
-
-
-    /**
-     * Replaces the entry for the specified key only if it is
-     * currently mapped to some value.
-     *
-     * @param key   key with which the specified value is associated
-     * @param value value to be associated with the specified key
-     * @return the previous value associated with the specified key, or
-     * {@code null} if there was no mapping for the key.
-     * (A {@code null} return can also indicate that the map
-     * previously associated {@code null} with the key,
-     * if the implementation supports null values.)
-     * @throws UnsupportedOperationException if the {@code put} operation
-     *                                       is not supported by this map
-     *                                       (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @throws ClassCastException            if the class of the specified key or value
-     *                                       prevents it from being stored in this map
-     *                                       (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @throws NullPointerException          if the specified key or value is null,
-     *                                       and this map does not permit null keys or values
-     * @throws IllegalArgumentException      if some property of the specified key
-     *                                       or value prevents it from being stored in this map
-     * @implSpec The default implementation is equivalent to, for this {@code map}:
-     *
-     * <pre> {@code
-     * if (map.containsKey(key)) {
-     *     return map.put(key, value);
-     * } else
-     *     return null;
-     * }</pre>
-     *
-     * <p>The default implementation makes no guarantees about synchronization
-     * or atomicity properties of this method. Any implementation providing
-     * atomicity guarantees must override this method and document its
-     * concurrency properties.
-     * @since 1.8
-     */
-    default V replace(K key, V value) {
-        V curValue;
-        if (((curValue = get(key)) != null) || containsKey(key)) {
-            curValue = put(key, value);
-        }
-        return curValue;
     }
 
 
