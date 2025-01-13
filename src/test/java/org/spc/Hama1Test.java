13736d094a978206f0c76592c7c750bac4f82c69
caowenxing <caowenxing@newland.com.cn>
2025-01-13 22:39:59 +0800

    基础重构测试 hamamap 1.0
    Signed-off-by: caowenxing <caowenxing@newland.com.cn>

---
+++ src/test/java/org/spc/Hama1Test.java
@@ -0,0 +1,19 @@
+package org.spc;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Hama1Test 测试初步重写的Hamamap, 还没加入自定义功能
+ */
+class Hama1Test {
+
+    /**
+     * 测试Hamamap基本功能
+     */
+    @Test
+    void testBasicFunc() {
+
+
+    }
+
+}
